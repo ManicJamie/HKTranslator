85838a3ae5d8a599c3096aacf017e5fdb6a4c38d
ManicJamie <30422476+ManicJamie@users.noreply.github.com>
2021-01-06 19:37:02 +0000

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HKTranslator;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while (true)
+            {
+                Console.WriteLine("Enter a scene name:");
+                input = Console.ReadLine();
+                Console.WriteLine("-> " + Translator.TranslateSceneName(input));
+            }
+        }
+    }
+}
