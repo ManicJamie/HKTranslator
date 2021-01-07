ade832b534f19962734e964fcb9a021ed737c42c
ManicJamie <30422476+ManicJamie@users.noreply.github.com>
2021-01-07 09:10:58 +0000

    Add mod details
    Added dependence on ModdingAPI

---
+++ HKTranslator/HKTranslator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Modding;
+
+namespace HKTranslator
+{
+    public class HKTranslator : Mod
+    {
+        public override string GetVersion() => "1.0";
+        public override int LoadPriority() => 1;
+    }
+}
