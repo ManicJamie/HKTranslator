9521f877c1e8e2a656d451a9690a743461b69067
ManicJamie <jamiebloomfield8@gmail.com>
2022-01-19 02:33:24 +0000

    Init project
    ugliest commit ever can't be bothered to branch from init lol

---
+++ HKTranslator/ModClass.cs
@@ -0,0 +1,36 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace HKTranslator1._5
+{
+    public class HKTranslator1._5 : Mod
+    {
+        internal static HKTranslator1._5 Instance;
+
+    //public override List<ValueTuple<string, string>> GetPreloadNames()
+    //{
+    //    return new List<ValueTuple<string, string>>
+    //    {
+    //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
+    //    };
+    //}
+
+    //public HKTranslator1._5() : base("HKTranslator1._5")
+    //{
+    //    Instance = this;
+    //}
+
+    public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+    {
+        Log("Initializing");
+
+        Instance = this;
+
+        Log("Initialized");
+    }
+}
+}
---
+++ HKTranslator/ReadMe.md
@@ -0,0 +1,3 @@
+# HKTranslator1._5
+
+A mod for the game Hollow Knight.
